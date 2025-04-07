dd6b44abea01bb27f82acf3e72002413eab1b308
Andrew Matsumura <42308941+glader0293@users.noreply.github.com>
2025-04-07 23:45:46 +0000

    Version Final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let arrCards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+	let randomNum = Math.floor(Math.random() * arrCards.length)
+  let newCard = arrCards[randomNum]
   
+  let accessNumber = document.getElementById("number")
+  accessNumber.innerHTML = `${newCard}`
+  
+  let suitRandom = ['spade', 'heart', 'club','diamond']
+  let randomSuit = Math.floor(Math.random() * suitRandom.length)
+  let newSuit = suitRandom[randomSuit]
+
+  let accessIconLeft = document.getElementById("icon-left")
+  let accessIconRight = document.getElementById("icon-right")
+  let spanLeft =  accessIconLeft.querySelector("span")
+  let spanRight =  accessIconRight.querySelector("span")
+
+    if(newSuit === 'spade'){
+      spanLeft.classList.add("spade")
+      spanLeft.innerHTML =("♠")
+      spanRight.classList.add("flipped-spade")
+      spanRight.innerHTML =("♠")
+    } else if(newSuit === "heart"){
+      spanLeft.classList.add("heart")
+      spanLeft.innerHTML =("♥")
+      spanRight.classList.add("flipped-heart")
+      spanRight.innerHTML =("♥")
+    } else if(newSuit === "club"){
+      spanLeft.classList.add("club")
+      spanLeft.innerHTML= ("♣")
+      spanRight.classList.add("flipped-club")
+      spanRight.innerHTML= ("♣")
+    } else if(newSuit === "diamond"){
+      spanLeft.classList.add("diamond")
+      spanLeft.innerHTML= ("♦")
+      spanRight.classList.add("flipped-diamond")
+      spanRight.innerHTML= ("♦")
+    }
+    
 };
